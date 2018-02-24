339ebd7203afffb8e795e769f532e0809cfe80ec
Taelor McClurg <tmcclurg@gmail.com>
2018-02-23 21:51:42 -0800

    adds compatibility with R's set.seed()

--- src/perm.cpp
+++ src/perm.cpp
@@ -1,24 +1,34 @@
 #include <Rcpp.h>
 using namespace Rcpp;
-// [[Rcpp::export]]
 
+double uniform(double u) {
+  /*
+   * This is a wrapper function around the Rcpp sugar 
+   * runif function. This is necesary, as the default
+   * random number generator used by std::random_shuffle
+   * does not work with R's set.seed()
+   */
+  return R::runif(0, u);                  
+}                            
+
+// [[Rcpp::export]]
 NumericMatrix perm(int n, NumericVector v){
   /*
-   * Takes a number n and vector v and returns a matrix of n rows, each of which
-   * is a permutation of the original vector. As far as I can tell, there is no
-   * native R way to do this without calling the sample function inside of a loop,
+   * Takes a number n and vector v and returns 
+   * a matrix of n rows, each of which
+   * is a permutation of the original vector. 
+   * As far as I can tell, there is no
+   * native R way to do this without calling 
+   * the sample function inside of a loop,
    * which is very slow.
-   * 
-   * ISSUE: Does not currently work with set.seed in R. 
    */
   int l = v.size();
   NumericMatrix m(n, l);
   for(int i=0; i<n; i++){
-    std::random_shuffle(v.begin(), v.end()); // Permutes v.
+    std::random_shuffle(v.begin(), v.end(), uniform); // Permutes v.
     for(int j=0; j<l; j++){                  
-      m(i, j) = v[j];                        // Fills a line of m with the permuted
-    }                                        // vector.
+      m(i, j) = v[j];                 // Fills a line of m with the permuted
+    }                                 // vector.
   }
   return m;
 }
-
